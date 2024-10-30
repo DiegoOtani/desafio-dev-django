da9a3261d62527cfae91ac606fecee67474e254e
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-10-29 22:33:47 -0300

    feat: add viewsets to create generic views

--- curriculos/views.py
+++ curriculos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import PersonalInfo, WorkExperience, AcademicTraining
+from .serializers import PersonalInfoSerializer, WorkExperienceSerializer, AcademicTrainingSerializer
 
-# Create your views here.
+class PersonalInfoViewSet(viewsets.ModelViewSet):
+  queryset = PersonalInfo.objects.all()
+  serializer_class = PersonalInfoSerializer
+
+class WorkExperienceViewSet(viewsets.ModelViewSet):
+  queryset = WorkExperience.objects.all()
+  serializer_class = WorkExperienceSerializer
+
+class AcademicTrainingViewSet(viewsets.ModelViewSet):
+  queryset = AcademicTraining.objects.all()
+  serializer_class = AcademicTrainingSerializer
