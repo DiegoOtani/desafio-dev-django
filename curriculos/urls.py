dd74fdafe25f5fce197893576eab73d21f1da64b
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-10-30 13:05:32 -0300

    feat: add ContactInfo view and configure urls

--- curriculos/urls.py
+++ curriculos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PersonalInfoViewSet, WorkExperienceViewSet, AcademicTrainingViewSet
+from .views import PersonalInfoViewSet, WorkExperienceViewSet, AcademicTrainingViewSet, ContactInfoViewSet
 
 router = DefaultRouter()
 router.register(r'personal-info', PersonalInfoViewSet)
+router.register(r'contact-info', ContactInfoViewSet)
 router.register(r'work-experience', WorkExperienceViewSet)
 router.register(r'academic-training', AcademicTrainingViewSet)
 
--- curriculos/views.py
+++ curriculos/views.py
@@ -1,12 +1,16 @@
 from rest_framework import viewsets
-from .models import PersonalInfo, WorkExperience, AcademicTraining
-from .serializers import PersonalInfoSerializer, WorkExperienceSerializer, AcademicTrainingSerializer
+from .models import PersonalInfo, WorkExperience, AcademicTraining, ContactInfo
+from .serializers import PersonalInfoSerializer, WorkExperienceSerializer, AcademicTrainingSerializer, ContactInfoSerializer
 from .pagination import PersonalInfoPagination
 
 class PersonalInfoViewSet(viewsets.ModelViewSet):
   queryset = PersonalInfo.objects.all()
   serializer_class = PersonalInfoSerializer
   pagination_class = PersonalInfoPagination
+
+class ContactInfoViewSet(viewsets.ModelViewSet):
+  queryset = ContactInfo.objects.all()
+  serializer_class = ContactInfoSerializer
 
 class WorkExperienceViewSet(viewsets.ModelViewSet):
   queryset = WorkExperience.objects.all()
