e9416ebea555880f32e5bf5468d5895c15bcf669
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-10-30 22:36:59 -0300

    feat: add DjangoAdmin

--- curriculos/admin.py
+++ curriculos/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import PersonalInfo, ContactInfo, WorkExperience, AcademicTraining
 
-# Register your models here.
+@admin.register(PersonalInfo)
+class PersonalInfoAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birth_date')
+    search_fields = ('first_name', 'last_name')
+
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    list_display = ('personal_info', 'email', 'phone')
+    search_fields = ('email',)
+
+@admin.register(WorkExperience)
+class WorkExperienceAdmin(admin.ModelAdmin):
+    list_display = ('position', 'company', 'start_date', 'end_date')
+    search_fields = ('position', 'company')
+
+@admin.register(AcademicTraining)
+class AcademicTrainingAdmin(admin.ModelAdmin):
+    list_display = ('institution', 'course', 'start_date', 'end_date')
+    search_fields = ('institution', 'course')
